1caef2cd530b97501bd40a4fdfb391e613bf9768
devin <devincorrigall@gmail.com>
2016-07-09 23:08:22 -0700

    #8 - updated android UI, add app icon

--- ARTS/app/src/main/java/com/uvic/arts/ARTSConstants.java
+++ ARTS/app/src/main/java/com/uvic/arts/ARTSConstants.java
@@ -1,7 +1,6 @@
 package com.uvic.arts;
 
 public class ARTSConstants {
-    public static final String INTENT_CONTENT = "arContent";
     public static final String CONTENT_DATA_KEY = "contentData";
     public static final String CONTENT_DATA = "content";
 }
--- ARTS/app/src/main/java/com/uvic/arts/ARToolkitActivity.java
+++ ARTS/app/src/main/java/com/uvic/arts/ARToolkitActivity.java
@@ -8,10 +8,12 @@ import android.util.Log;
 import android.widget.FrameLayout;
 
 import org.artoolkit.ar.base.ARActivity;
+import org.artoolkit.ar.base.ARToolKit;
 import org.artoolkit.ar.base.rendering.ARRenderer;
 
 public class ARToolkitActivity extends ARActivity{
     private Bitmap bitmap;
+    private boolean firstUpdate = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,5 +33,42 @@ public class ARToolkitActivity extends ARActivity{
     @Override
     protected FrameLayout supplyFrameLayout() {
         return (FrameLayout) this.findViewById(R.id.aRToolkitFrameLayout);
+    }
+
+    // Override to remove toast being displayed when camera opens
+    @Override
+    public void cameraPreviewStarted(int width, int height, int rate, int cameraIndex, boolean cameraIsFrontFacing) {
+        if(ARToolKit.getInstance().initialiseAR(width, height, "Data/camera_para.dat", cameraIndex, cameraIsFrontFacing)) {
+            Log.i("ARActivity", "Camera initialised");
+        } else {
+            Log.e("ARActivity", "Error initialising camera. Cannot continue.");
+            this.finish();
+        }
+
+        this.firstUpdate = true;    }
+
+    // Override to handle the first update to the camera, since we're not able to set firstUpdate from
+    // outside of ARActivity
+    @Override
+    public void cameraPreviewFrame(byte[] frame) {
+        if(this.firstUpdate) {
+            if(this.renderer.configureARScene()) {
+                Log.i("ARActivity", "Scene configured successfully");
+            } else {
+                Log.e("ARActivity", "Error configuring scene. Cannot continue.");
+                this.finish();
+            }
+
+            this.firstUpdate = false;
+        }
+
+        super.cameraPreviewFrame(frame);
+    }
+
+    // Try and avoid having ARActivity open when pressing back
+    @Override
+    public void onBackPressed() {
+        super.finish();
+        this.finish();
     }
 }
