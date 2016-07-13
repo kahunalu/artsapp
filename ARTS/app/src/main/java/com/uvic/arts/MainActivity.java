7f9d1804208180b4e5c387a9926a39770734154a
devin <devincorrigall@gmail.com>
2016-07-13 13:46:22 -0700

    Tried to stop UI freezing when opening large images, doesn't quite work, but nbd

--- ARTS/app/src/main/java/com/uvic/arts/MainActivity.java
+++ ARTS/app/src/main/java/com/uvic/arts/MainActivity.java
@@ -11,8 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
-
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -93,7 +91,32 @@ public class MainActivity extends AppCompatActivity {
                 // otherwise it will fail to initialize, since we were just using the camera
                 // to read the QR code. Hacky, but it works.
                 Thread.sleep(1000);
-                return requestManager.getContent(contentIds[0]);
+                String response = requestManager.getContent(contentIds[0]);
+
+                JSONObject jsonObject;
+                String size = null;
+
+                try {
+                    jsonObject = new JSONObject(response);
+
+                    // Get image size
+                    size = jsonObject.getString(ARTSConstants.CONTENT_SIZE_KEY);
+
+                    // Save imageData to a file, since it can be too big to pass in an intent
+                    String imageData = jsonObject.getString(ARTSConstants.CONTENT_DATA_KEY);
+                    File file = new File(MainActivity.this.getFilesDir(), ARTSConstants.CONTENT_DATA_FILENAME);
+                    FileOutputStream outputStream = new FileOutputStream(file);
+                    outputStream.write(imageData.getBytes());
+
+                } catch (JSONException | IOException exception) {
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                    }
+
+                    Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+                return size;
             } catch (IOException ioException) {
                 Toast.makeText(MainActivity.this, "Error retrieving content", Toast.LENGTH_LONG).show();
             } catch (InterruptedException e) {
@@ -103,33 +126,19 @@ public class MainActivity extends AppCompatActivity {
             return "";
         }
 
-        protected void onPostExecute(String response) {
-            JSONObject jsonObject;
-
+        protected void onPostExecute(String contentSize) {
             if (progressDialog.isShowing()) {
                 progressDialog.dismiss();
             }
 
-            try {
-                jsonObject = new JSONObject(response);
+            // Create ARToolkit activity intent
+            Intent intentARToolkitActivity = new Intent(MainActivity.this, ARToolkitActivity.class);
 
-                // Create ARToolkit activity intent
-                Intent intentARToolkitActivity = new Intent(MainActivity.this, ARToolkitActivity.class);
+            // Add content size to the intent
+            intentARToolkitActivity.putExtra(ARTSConstants.CONTENT_SIZE, contentSize);
 
-                // Save imageData to a file, since it can be too big to pass in an intent
-                String imageData = jsonObject.getString(ARTSConstants.CONTENT_DATA_KEY);
-                File file = new File(MainActivity.this.getFilesDir(), ARTSConstants.CONTENT_DATA_FILENAME);
-                FileOutputStream outputStream = new FileOutputStream(file);
-                outputStream.write(imageData.getBytes());
-
-                // Add content size to the intent
-                intentARToolkitActivity.putExtra(ARTSConstants.CONTENT_SIZE, jsonObject.getString(ARTSConstants.CONTENT_SIZE_KEY));
-
-                // Start Activity
-                startActivity(intentARToolkitActivity);
-            } catch (JSONException | IOException exception) {
-                Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
-            }
+            // Start Activity
+            startActivity(intentARToolkitActivity);
         }
     }
 }
